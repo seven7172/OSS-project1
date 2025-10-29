512d67e763e0e44dc521edf8e9be79de68121426
seven7172 <seven7172@naver.com>
2025-10-29 15:03:39 +0900

    Fix syntax errors and add minus function
    Fixed syntax errors and added a minus function.

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 int plus(int x, int y){
 return x+y;
 }
+int minus(int x, int y){
+return x-y;
+}
 
 int main(){
   printf("Hello World");
   printf("Firtst GitHub Training");
-  z = plus(2,3)
+  z = plus(2,3);
+  k = minus(5,4);
   return 0;
 
 
